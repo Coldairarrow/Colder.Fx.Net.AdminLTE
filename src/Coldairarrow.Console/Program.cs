d11f4cd17e52aa83650160e9fa503563cb538d1a
Coldairarrow <xgc15657519397@outlook.com>
2019-03-05 16:42:23 +0800

    更新

--- src/Coldairarrow.Console/Program.cs
+++ src/Coldairarrow.Console/Program.cs
@@ -4,13 +4,8 @@ namespace Coldairarrow.Console1
 {
     class Program
     {
-        class Test
-        {
-            public string MyProperty { get;}
-        }
         static void Main(string[] args)
         {
-            var propperty = typeof(Test).GetProperty("MyProperty");
             Console.WriteLine("完成");
             Console.ReadLine();
         }
