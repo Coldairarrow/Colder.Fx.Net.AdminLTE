ec868410ec446b314a3b854312392d36e5077679
Coldairarrow <xgc15657519397@outlook.com>
2019-03-12 22:01:14 +0800

    更新

--- src/Coldairarrow.Web/Controllers/TestController.cs
+++ src/Coldairarrow.Web/Controllers/TestController.cs
@@ -1,34 +1,12 @@
-﻿using Coldairarrow.Business;
-using Coldairarrow.DotNettyRPC;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Coldairarrow.Web
 {
-    public interface IHello
-    {
-        string SayHello(string msg);
-    }
-    public class Hello : IHello
-    {
-        public string SayHello(string msg)
-        {
-            return msg;
-        }
-    }
-
     public class TestController : BaseController
     {
         public ActionResult Index()
         {
             return View();
-        }
-
-        [CheckParamNotEmpty("aa")]
-        public ActionResult Test()
-        {
-
-
-            return Success();
         }
     }
 }
