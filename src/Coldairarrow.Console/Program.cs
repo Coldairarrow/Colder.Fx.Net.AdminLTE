02128725819268c934f1ec97d720a397787d9fe6
Coldairarrow <Coldairarrow@DESKTOP-8OFRDO2>
2019-04-07 21:37:06 +0800

    优化

--- src/Coldairarrow.Console/Program.cs
+++ src/Coldairarrow.Console/Program.cs
@@ -7,6 +7,7 @@ using Coldairarrow.Business.Base_SysManage;
 using Coldairarrow.Entity.Base_SysManage;
 using System.Collections.Generic;
 using System.Linq.Dynamic;
+using System.Diagnostics;
 
 namespace Coldairarrow.Console1
 {
@@ -15,20 +16,6 @@ namespace Coldairarrow.Console1
         static void Main(string[] args)
         {
             Base_UserBusiness base_UserBusiness = new Base_UserBusiness();
-            List<string> ids = new List<string>() { "Admin" };
-            //var list = base_UserBusiness.GetIQueryable().Where("@0.Contains(outerIt.UserId)", ids).ToList();
-            //List<Base_User> insertList = new List<Base_User>();
-            //LoopHelper.Loop(100, index =>
-            //{
-            //    insertList.Add(new Base_User
-            //    {
-            //        Id = GuidHelper.GenerateKey(),
-            //        UserId = GuidHelper.GenerateKey(),
-            //        UserName = $"名字{index}",
-            //        RealName = $"名字{index}"
-            //    });
-            //});
-            //base_UserBusiness.Insert(insertList);
 
             Console.WriteLine("完成");
             Console.ReadLine();
