1a0a6167cfff5be763460fb8f8643506e4389ba8
Coldairarrow <xgc15657519397@outlook.com>
2019-04-18 14:04:07 +0800

    文件上传优化

---
+++ src/Coldairarrow.Web/App_Start/01Handler/POST请求过滤/PostRequestAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Coldairarrow.Util;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Coldairarrow.Web
+{
+    public class PostRequestAttribute : FilterAttribute, IActionFilter
+    {
+        /// <summary>
+        /// Action执行之前执行
+        /// </summary>
+        /// <param name="filterContext">过滤器上下文</param>
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.HttpMethod.ToLower() != "post")
+            {
+                ErrorResult res = new ErrorResult
+                {
+                    Msg = "仅支持POST方法"
+                };
+                filterContext.Result = new ContentResult { Content = res.ToJson(), ContentEncoding = Encoding.UTF8 };
+            }
+        }
+
+        /// <summary>
+        /// Action执行完毕之后执行
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+        }
+    }
+}
--- src/Coldairarrow.Web/Areas/Base_SysManage/Controllers/CommonController.cs
+++ src/Coldairarrow.Web/Areas/Base_SysManage/Controllers/CommonController.cs
@@ -1,4 +1,7 @@
 ﻿using Coldairarrow.Util;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace Coldairarrow.Web
@@ -21,6 +24,42 @@ namespace Coldairarrow.Web
                 src = url
             };
             return JsonContent(res.ToJson());
+        }
+
+        public ActionResult UploadFile(string fileName, string data, string fileType)
+        {
+            string fileBase64 = GetBase64String(data);
+            byte[] bytes = fileBase64.ToBytes_FromBase64Str();
+
+            string relativeDir = $"/Upload/File/{Guid.NewGuid().ToString()}";
+            string absoluteDir = System.Web.HttpContext.Current.Server.MapPath($"~{relativeDir}");
+            if (!Directory.Exists(absoluteDir))
+                Directory.CreateDirectory(absoluteDir);
+            string filePath = Path.Combine(absoluteDir, fileName);
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                using (MemoryStream m = new MemoryStream(bytes))
+                {
+                    m.WriteTo(fileStream);
+                }
+            }
+
+            string url = $"{GlobalSwitch.WebRootUrl}{relativeDir}/{fileName}";
+
+            var res = new
+            {
+                success = true,
+                src = url
+            };
+            return JsonContent(res.ToJson());
+
+            string GetBase64String(string base64Url)
+            {
+                string parttern = "^.*?base64,(.*?)$";
+                var match = Regex.Match(base64Url, parttern);
+
+                return match.Groups[1].ToString();
+            }
         }
     }
 }
