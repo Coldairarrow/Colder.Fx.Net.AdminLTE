7a51a1ff0d0c7554900fe0aeb7ae81e72ac2e646
Coldairarrow <xgc15657519397@outlook.com>
2019-05-06 17:30:54 +0800

    雪花算法

---
+++ src/Coldairarrow.Util/ClassLibrary/Snowflake/SnowflakeId.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coldairarrow.Util.Snowflake
+{
+    public struct SnowflakeId
+    {
+        static SnowflakeId()
+        {
+            _idWorker = new IdWorker(1, 1);
+        }
+        private static IdWorker _idWorker { get; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
