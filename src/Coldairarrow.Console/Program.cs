5d7c337d85f15e80af30bf18a5d66f16dc0e5e57
Coldairarrow <xgc15657519397@outlook.com>
2019-06-18 17:23:45 +0800

    更新

--- src/Coldairarrow.Console/Program.cs
+++ src/Coldairarrow.Console/Program.cs
@@ -18,11 +18,48 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using System.Threading;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
 
 namespace Coldairarrow.Console1
 {
     class Program
     {
+        static Program()
+        {
+            var builder = new ContainerBuilder();
+
+            var baseType = typeof(IDependency);
+            var baseTypeCircle = typeof(ICircleDependency);
+
+            //Coldairarrow相关程序集
+            var assemblys = Assembly.GetEntryAssembly().GetReferencedAssemblies()
+                .Select(Assembly.Load)
+                .Cast<Assembly>()
+                .Where(x => x.FullName.Contains("Coldairarrow")).ToList();
+
+            //自动注入IDependency接口,支持AOP
+            builder.RegisterAssemblyTypes(assemblys.ToArray())
+                .Where(x => baseType.IsAssignableFrom(x) && x != baseType)
+                .AsImplementedInterfaces()
+                .PropertiesAutowired()
+                .InstancePerLifetimeScope()
+                .EnableInterfaceInterceptors()
+                .InterceptedBy(typeof(Interceptor));
+
+            //自动注入ICircleDependency接口,循环依赖注入,不支持AOP
+            builder.RegisterAssemblyTypes(assemblys.ToArray())
+                .Where(x => baseTypeCircle.IsAssignableFrom(x) && x != baseTypeCircle)
+                .AsImplementedInterfaces()
+                .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies)
+                .InstancePerLifetimeScope();
+
+            //AOP
+            builder.RegisterType<Interceptor>();
+
+            AutofacHelper.Container = builder.Build();
+        }
+
         static void ShardingTest()
         {
             var db = DbFactory.GetRepository();
